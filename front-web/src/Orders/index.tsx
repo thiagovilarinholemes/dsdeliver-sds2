85b95bb930a27c3818f5ea1a0f8bb43bab5ca1ef
Thiago V. Lemes <lemes_vilarinho@yahoo.com.br>
2021-01-07 10:27:20 -0300

    Create API

--- front-web/src/Orders/index.tsx
+++ front-web/src/Orders/index.tsx
@@ -1,11 +1,25 @@
+import { useState, useEffect } from 'react';
 import React from 'react';
 import './styles.css';
 import StepsHeader from './StepsHeader';
+import ProductsList from './ProductsList';
+import { Product } from './types';
+import { fetchProducts } from './api';
 
 function Orders(){
+
+    const [products, setProducts] = useState<Product[]>([]);
+
+    useEffect(() =>{
+        fetchProducts()
+            .then(reponse => setProducts(reponse.data))
+            .catch(error => console.log(error));
+    }, []);
+
     return(
         <div className="orders-container">
             <StepsHeader />
+            <ProductsList products={products}/>
         </div>
     )
 }
